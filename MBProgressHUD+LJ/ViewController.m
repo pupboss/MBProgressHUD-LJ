2726580416588eadc7512e0c924e71faf6823805
高考这件小事 <pupboss.vip@gmail.com>
2015-03-16 21:23:45 +0800

    source code

--- MBProgressHUD+LJ/ViewController.m
+++ MBProgressHUD+LJ/ViewController.m
@@ -7,12 +7,37 @@
 //
 
 #import "ViewController.h"
+#import "MBProgressHUD+LJ.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+#pragma mark IBAction methods
+
+- (IBAction)showMsg {
+    
+    [MBProgressHUD showMessage:@"请骚等一会儿"];
+    
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+       
+        [MBProgressHUD hideHUD];
+    });
+}
+
+- (IBAction)showSuccess {
+
+    [MBProgressHUD showSuccess:@"登录成功"];
+}
+
+- (IBAction)showError {
+    
+    [MBProgressHUD showError:@"服务器被外星人搬走了o(╯□╰)o"];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
